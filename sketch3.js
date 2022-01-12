a0b4585baca12df9e6f6102fd1c140dba9b25a3d
FlaviaMatsuoka <97564017+FlaviaMatsuoka@users.noreply.github.com>
2022-01-11 21:36:23 -0400

    Add files via upload

---
+++ sketch3.js
@@ -0,0 +1,18 @@
+var caixa ;
+function setup() {
+  createCanvas(400,400);
+caixa=createSprite(200,200,50,50);
+caixa.shapeColor="blue";
+
+
+}
+
+function draw() 
+{
+  background("green");
+drawSprites();
+}
+
+
+
+
